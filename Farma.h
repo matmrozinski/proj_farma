2dcef06aec84920d8574bd4f44aeb89e3cae765a
student <student@debian>
2016-06-08 11:33:50 +0200

    Zmiany

--- Farma.h
+++ Farma.h
@@ -1,22 +1,37 @@
 #pragma once
 
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 class Farmer
 {
 	public:
-	long long money;
+	long long money=0;
 	string name;
-	parametr inteligence () {return_inteligence};
+	// parametr inteligence () {return_inteligence}
 
 };
 
-struct Farma
+class Farma
 {
-	int liczba_kur;
-	int liczba_krow;
-	int liczba_jajek;
+	public:
+	int liczba_kur=1;
+	int liczba_krow=0;
+	int liczba_jajek=0;
+};
 
-}
+
+class kura
+{
+public:
+	int prawd_jajka;
+	kura( int prawd ){prawd_jajka=prawd;}
+
+};
+
+struct krowa
+{
+
+};
--- main.cpp
+++ main.cpp
@@ -2,12 +2,61 @@
 #include <iostream>
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-using namesace std;
+using namespace std;
 
-int main(int argc, char const *argv[])
+
+
+int main(int argc, char const **argv)
 {
+
+	
 	srand(time(NULL));
-	Farma.liczba_kur=5;
-	Farma.liczba krow=0;
-	Farma.liczba_jajek=0;
+	Farma *farma;
+	Farmer *farmer;
+	
+	long long goal_money;
+	
+	
+	cout<<"Podaj Liczbe kur"<<endl;
+	cin>> farma.liczba_kur;
+	cout<<"Podaj, ile pieniedzy chcesz zarobic"<<endl;
+	cin>> goal_money;
+
+	
+	int turn_limit=500;
+	
+	
+
+	for(int i=0; i<turn_limit; i++)
+	{
+		if(farmer.money >= goal_money)
+		{
+		cout<<"gratulacje, wygrales";
+		return 0;
+		}
+		for(int j=0; j<farma.liczba_kur; j++)	//kury znosza jajka z ktorych moga sie wykluc inne kury 
+		{
+			int p= 1 + rand() % 100;
+			if(p<21)
+			farma.liczba_jajek++;
+			else if (p>20 && p<25)
+			farma.liczba_kur++;
+		}
+		if(farma.liczba_jajek>=50)	//sprzedaz jajek i kupno krowy	
+		{
+		farma.liczba_jajek-=50;
+		farma.liczba_krow++;
+		}
+
+		for(int y=0; y<farma.liczba_krow; y++)	//krowy dają mleko za ktore farmer otrzymuje pieniadze
+		{
+		farmer.money+=10;
+		}
+		usleep(1000000);
+	}
+		
+		
+}
